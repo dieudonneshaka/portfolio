2a03d76a158aa1a72440a83da160278eb587e196
dieudonneshaka <158247669+dieudonneshaka@users.noreply.github.com>
2026-06-25 00:58:07 +0300

    pudated

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -4,45 +4,52 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
-
 const Navbar = () => {
-  return (
-    <nav className="navbar">
-      <div className="logo">GE</div>
+  
+  return(
+        <nav className="navbar">
+          <div className="log">
+          </div>
+          
       <ul className="nav-links">
         <li><a href="#about">About</a></li>
         <li><a href="#speaking">Speaking</a></li>
         <li><a href="#writing">Writing</a></li>
         <li><a href="#uses">Uses</a></li>
+        <button> 
+        <li><a href="#work">Work</a></li>
+        <li><a href="#about">Abouut</a></li>
+        <li><a href="#experiance">Experiance</a></li>
         <li><a href="#contact">Contact</a></li>
+        </button>
       </ul>
     </nav>
+
+
+    
   )
 }
 
 const App = () => {
   return (
-    <div className="page">
+
+    <div className= "page" >
       <Navbar />
 
-      <section className="hero">
-        <div className="hero-text">
-          <h1>Hello, I'm Dieudonne</h1>
-          <p>I'm a software engineer based in Finland.</p>
-          <ul className="socials">
-            <li><a href="#">TW</a></li>
-            <li><a href="#">GH</a></li>
-            <li><a href="#">IG</a></li>
-            <li><a href="#">LI</a></li>
-          </ul>
-        </div>
+      <h1>welcome home</h1>
+      <section className="yeye">
+        <div className="hero"></div>
+        <ul className="socials">
+          <li><a href="#We">we</a></li>
+          <li><a href="#DE">DE</a></li>
+          <li><a href="#LI">LI</a></li>
 
-        <img
-          src="."
-          alt="Dieudonne"
-          className="hero-image"
-        />
+        </ul>
+
+
       </section>
+      
+
     </div>
   )
 }
